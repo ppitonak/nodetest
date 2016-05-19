fd38cc9e46f2d5dcbd120ff52c3a897c9321968b
Pavol Pitonak <pavol@pitonak.com>
2016-05-19 11:56:09 +0200

    added a dummy unit test with a typo

---
+++ test/simple.js
@@ -0,0 +1,9 @@
+let expect = require('chai').expect;
+
+describe('Dummy unit tests', function() {
+
+  it('should correctly compute sum of 1 and 1', function() {
+      expect(1+1).to.equal(3);
+  });
+
+});
