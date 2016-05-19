441a1127c789ab584caf0d953320bfbf3e5e1362
Pavol Pitonak <pavol@pitonak.com>
2016-05-19 15:13:10 +0200

    test updated (#12)

--- test/simple.js
+++ test/simple.js
@@ -6,8 +6,8 @@ describe('Dummy unit tests', function() {
       expect(1+1).to.equal(2);
   });
   
-  it('should correctly compute sum of 2 and 3', function() {
-      expect(2+3).to.equal(5);
+  it('should correctly compute sum of 2 and 4', function() {
+      expect(2+4).to.equal(6);
   });
 
   it('should correctly compute product of 2 and 3', function() {
