9d7522abcb378e05384cfa7071d533843d5d7c44
palino <snayper@gmail.com>
2016-05-19 14:39:42 +0200

    new test

--- test/simple.js
+++ test/simple.js
@@ -5,6 +5,10 @@ describe('Dummy unit tests', function() {
   it('should correctly compute sum of 1 and 1', function() {
       expect(1+1).to.equal(2);
   });
+  
+  it('should correctly compute sum of 2 and 3', function() {
+      expect(2+3).to.equal(5);
+  });
 
   it('should correctly compute product of 2 and 3', function() {
       expect(2*3).to.equal(6);
